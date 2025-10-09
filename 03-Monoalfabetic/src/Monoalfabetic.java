6130a1d7cfed71e7221ba07a761e63e2c190be0a
Izan <izan.hidalgo2006@gmail.com>
2025-10-09 18:40:35 +0200

    Exercici 3 Monoalfabetic fet

--- 03-Monoalfabetic/src/Monoalfabetic.java
+++ 03-Monoalfabetic/src/Monoalfabetic.java
@@ -1,72 +1,127 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Monoalfabetic {
-    static final char[] alfabet = "AÀÁBCÇDEÈÉFGHIÏJKLMNÑOÓPQRSTUÜVWXYZ".toCharArray();
+    private final char[] alfabet = "AÀÁBCÇDEÈÉFGHIÏJKLMNÑOÓPQRSTUÜVWXYZ".toCharArray();
     
-    static char[] permutat;
-
+    private char[] permutat;
 
     public Monoalfabetic() {
       this.permutat = permutaAlfabet();
     }
 
+
     public char[] permutaAlfabet() {
       List<Character> lista = new ArrayList<>();
-      
-      for (char c : alfabet) {
-        lista.add(c);
-      
-      }
-      Collections.shuffle(lista);
-      char[] perm = new char[lista.size()];
-      
-      for (int i = 0; i < lista.size(); i++) {
-        perm[i] = lista.get(i);
-      }
-      return perm;
+        
+        // pasamos todos los caracteres del array alfabet a la lista
+        for (char c : alfabet) {
+            lista.add(c);
+        }
+
+        // mezclamos la lista con shuffle, que lo que hace es barajar los elementos
+        Collections.shuffle(lista);
+
+        // creamos un nuevo array del mismo tamaño que la lista
+        char[] perm = new char[lista.size()];
+
+        // pasamos los elementos mezclados de la lista al array este
+        for (int i = 0; i < lista.size(); i++) {
+            perm[i] = lista.get(i);
+        }
+
+        return perm; 
     }
 
     public String xifraMonoAlfa(String cadena) {
-      String resultat = "";
+      String resultat = ""; 
 
-      for (char c : cadena.toCharArray()) {
-        boolean trobat = false;
-      }
+        // recorremos cada caracter de la cadena original
+        for (char c : cadena.toCharArray()) {
+            boolean trobat = false; // para saber si hemos encontrado la letra en el alfabeto
 
-        for (int i = 0; i < alfabet; i+) {
-          if (c == alfabet[i]) {
-            resultat = resultat + permutat[i];
-            trobat = true;
-            break;
-          }
+            // comprobamos si el caracter es mayúscula
+            for (int i = 0; i < alfabet.length; i++) {
+                if (c == alfabet[i]) { 
+                    resultat = resultat + permutat[i]; 
+                    trobat = true;
+                    break; 
+                }
+            }
+
+            // si no era mayúscula, comprobamos si es minúscula
+            if (!trobat) {
+                char upper = Character.toUpperCase(c); 
+                for (int i = 0; i < alfabet.length; i++) {
+                    if (upper == alfabet[i]) {
+                        // si coincide, añadimos la versión minúscula de la letra cifrada
+                        resultat = resultat + Character.toLowerCase(permutat[i]);
+                        trobat = true;
+                        break;
+                    }
+                }
+            }
+
+            // si no se ha encontrado espacio, número, símbolo o lo que sea, se deja igual
+            if (!trobat) {
+                resultat = resultat + c;
+            }
         }
-
-        if (!trobat) {
-          char upper = Character.toUpperCase(c)
-            for (i = 0; i < )
-        }
-
+        return resultat;
     } 
 
     public String desxifraMonoAlfa(String cadena) {
-      return null;
+      String resultat = ""; // texto descifrado
+
+        for (char c : cadena.toCharArray()) {
+            boolean trobat = false;
+
+            // compruebo si el carácter es mayuscula cifrada
+            for (int i = 0; i < permutat.length; i++) {
+                if (c == permutat[i]) {
+                    resultat = resultat + alfabet[i]; 
+                    trobat = true;
+                    break;
+                }
+            }
+
+            // si no era mayuscula, comprobamos si es minúscula
+            if (!trobat) {
+                char upper = Character.toUpperCase(c);
+                for (int i = 0; i < permutat.length; i++) {
+                    if (upper == permutat[i]) {
+                        resultat = resultat + Character.toLowerCase(alfabet[i]);
+                        trobat = true;
+                        break;
+                    }
+                }
+            }
+
+            // si no está en el alfabeto, se deja igual
+            if (!trobat) {
+                resultat = resultat + c;
+            }
+        }
+
+        return resultat;
     }
     
     
     public static void main(String[] args) {
-      permutat = permutaAlfabet();
-      System.out.println("Alfabet original: " + Arrays.toString(alfabet));
-      System.out.println("Alfabet permutat: " + Arrays.toString(permutat));
+      Monoalfabetic mono = new Monoalfabetic();
 
-      String text = "Hola Món";
-      String xifrat = xifraMonoAlfa(text);
-      String desxifrat = desxifraMonoAlfa(xifrat);
+      System.out.println("Alfabet original: " + Arrays.toString(mono.alfabet));
+      System.out.println("Alfabet permutat: " + Arrays.toString(mono.permutat));
 
-      System.out.println("Text original = " + text);
-      System.out.println("Text xifrat = " + xifrat);
+      String text = "Hola Món!";
+      String xifrat = mono.xifraMonoAlfa(text);
+      String desxifrat = mono.desxifraMonoAlfa(xifrat);
+
+      System.out.println("Text original  = " + text);
+      System.out.println("Text xifrat    = " + xifrat);
       System.out.println("Text desxifrat = " + desxifrat);
-        
     }
 }
